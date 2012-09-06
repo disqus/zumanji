67bdd9b60385688bd04736812c49efd07cb924c0
David Cramer <dcramer@gmail.com>
2012-09-06 15:51:06 -0700

    Add another decimal place to sparklines

--- src/zumanji/static/zumanji/js/zumanji.js
+++ src/zumanji/static/zumanji/js/zumanji.js
@@ -1,7 +1,7 @@
 Sparkline = (function(options){
 
   function floatFormat(value){
-    return Math.round(value * 100) / 100;
+    return Math.round(value * 1000) / 1000;
   }
 
   var $el = $('<span class="sparkline"></span>').css({
