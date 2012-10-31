faaa21f832702620e1ccbe0094907e1e25601915
David Cramer <dcramer@gmail.com>
2012-10-31 16:16:24 -0700

    0.3.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ install_requires = [
 
 setup(
     name='zumanji',
-    version='0.3.0',
+    version='0.3.1',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='https://github.com/disqus/zumanji',
